95b469ee4cdfd9c8fe37e750417dc5fb4e383db8
Bencuska <rizb@kkszki.hu>
2025-09-24 08:59:38 +0200

    Saving

--- Osztalyok1/Program.cs
+++ Osztalyok1/Program.cs
@@ -8,6 +8,25 @@ namespace Osztalyok1
 {
     internal class Program
     {
+        public class Butorlap
+        {
+            private int hosszusag;
+            private int szelesseg;
+
+            const int tartolapnegyzetar = 5000;
+            const int hatlapnegyzetar = 500;
+
+            public int Tartolap
+            {
+                get { return hosszusag; }
+                set { hosszusag = value; }
+            }
+            public int Hatlap
+            {
+                get { return szelesseg; }
+                set { szelesseg = value; }
+            }
+        }
         static void Main(string[] args)
         {
         }
