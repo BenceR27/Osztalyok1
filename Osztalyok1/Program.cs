d6a63ef5fb0920315e869496b450dea0c33a064c
Bence Riz <rizbence@gmail.com>
2025-09-30 21:40:49 +0200

    ArSzamol Class has been created and functioning

--- Osztalyok1/Program.cs
+++ Osztalyok1/Program.cs
@@ -14,14 +14,31 @@ namespace Osztalyok1
             private double szelesseg; 
             private string tipus;     
 
-            const int tartolapnegyzetar = 5000;
-            const int hatlapnegyzetar = 500;
+            const int tartolap_ar = 5000;
+            const int hatlap_ar = 500;
 
             public Butorlap(string tipus, double hosszCm, double szelessegCm)
             {
                 this.tipus = tipus;
                 this.hossz = hosszCm / 100.0;
                 this.szelesseg = szelessegCm / 100.0;
+            }
+
+            public double ArSzamol()
+            {
+                double terulet = hossz * szelesseg; 
+                if (tipus.ToLower() == "tartolap")
+                {
+                    return terulet * tartolap_ar;
+                }
+                else if (tipus.ToLower() == "hatlap")
+                {
+                    return terulet * hatlap_ar;
+                }
+                else
+                {
+                    return 0; 
+                }
             }
         }
         static void Main(string[] args)
